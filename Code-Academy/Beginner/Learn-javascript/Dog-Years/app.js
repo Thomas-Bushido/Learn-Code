fb24b25c7a7b3b1ed0b24d08638ebb14e0dcfd05
Thomas-Bushido <thomas.pereira59@outlook.fr>
2024-12-10 14:50:35 +0100

    Dog Years

---
+++ Code-Academy/Beginner/Learn-javascript/Dog-Years/app.js
@@ -0,0 +1,19 @@
+let myAge = 32;
+// comment
+let earlyYears = 2;
+// comment
+earlyYears *= 10.5;
+
+let laterYears = myAge -2;
+// comment
+laterYears *= 4;
+// comment
+console.log(`${earlyYears} and ${laterYears}.`)
+// comment
+
+myAgeInDogYears = earlyYears + laterYears ;
+// comment
+
+const myName = 'thomas'.toLowerCase();
+// comment
+console.log(`my name is ${myName} i am ${myAge} years old in human years wich is ${myAgeInDogYears} years old in dog years`);
